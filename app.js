313b703674d503bdc0416316274e083022f760d2
Lijuan Wang <2738042w@student.gla.ac.uk>
2024-10-10 23:54:50 +0100

    init project

---
+++ app.js
@@ -0,0 +1,8 @@
+
+// const log = require('./logger')
+
+// log.log('message')
+
+const path = require('path')
+const pathObj = path.parse(__filename)
+console.log(pathObj)
---
+++ logger.js
@@ -0,0 +1,7 @@
+const url = 'http://logger.com/log'
+
+function log(message){
+    console.log(message)
+}
+
+module.exports.log = log;
